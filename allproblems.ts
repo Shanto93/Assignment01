2728337d18a326f36bdfb5631e70382f2934f084
Shanto93 <shantoislam7363@gmail.com>
2025-05-07 19:24:46 +0600

    Implement solution for Problem 2

--- allproblems.ts
+++ allproblems.ts
@@ -1,5 +1,6 @@
- //Problem 1
- function formatString(input: string, toUpper?: boolean): string {
+{
+  //Problem 1
+  function formatString(input: string, toUpper?: boolean): string {
     if (toUpper === true) {
       return input.toUpperCase();
     } else if (toUpper === false) {
@@ -11,3 +12,26 @@
 
   const result = formatString("Hello", true);
   console.log(result);
+}
+
+{
+  //Problem 2
+  function filterByRating(
+    items: { title: string; rating: number }[]
+  ): { title: string; rating: number }[] {
+    return items
+      .filter((item) => item.rating >= 4.0)
+      .map((item) => {
+        return { title: item.title, rating: item.rating };
+      });
+  }
+
+  const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 },
+  ];
+
+  const result = filterByRating(books);
+  console.log(result);
+}
