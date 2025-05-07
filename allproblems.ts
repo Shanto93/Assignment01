8389af412f2e6e77c687780f7f70a97f865d4895
Shanto93 <shantoislam7363@gmail.com>
2025-05-07 19:12:03 +0600

    Implement solution for Problem 1

--- allproblems.ts
+++ allproblems.ts
@@ -1,1 +1,13 @@
-const start = "Hello";
+ //Problem 1
+ function formatString(input: string, toUpper?: boolean): string {
+    if (toUpper === true) {
+      return input.toUpperCase();
+    } else if (toUpper === false) {
+      return input.toLowerCase();
+    } else {
+      return input.toUpperCase();
+    }
+  }
+
+  const result = formatString("Hello", true);
+  console.log(result);
