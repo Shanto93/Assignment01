c4174c6e7fb7dd83e0f06307bf7fb23fcc3ce30e
Shanto93 <shantoislam7363@gmail.com>
2025-05-07 20:55:17 +0600

    Implement solution for Problem 7

--- allproblems.ts
+++ allproblems.ts
@@ -113,3 +113,26 @@
 
   getMostExpensiveProduct(products);
 }
+
+{
+  //Problem 7
+  enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+  }
+
+  function getDayType(day: Day): string {
+    if (day === Day.Saturday || day === Day.Sunday) {
+      return "Weekend";
+    } else {
+      return "Weekday";
+    }
+  }
+
+  getDayType(Day.Friday);
+}
