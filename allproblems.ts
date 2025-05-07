260fcce421e30b9cbaa64db54771705ce422e924
Shanto93 <shantoislam7363@gmail.com>
2025-05-07 20:00:44 +0600

    Implement solution for Problem 4

--- allproblems.ts
+++ allproblems.ts
@@ -10,8 +10,7 @@
     }
   }
 
-  const result = formatString("Hello", true);
-  console.log(result);
+  formatString("Hello", true);
 }
 
 {
@@ -32,8 +31,7 @@
     { title: "Book C", rating: 5.0 },
   ];
 
-  const result = filterByRating(books);
-  console.log(result);
+  filterByRating(books);
 }
 
 {
@@ -42,6 +40,34 @@
     return arrays.reduce((acc, array) => acc.concat(array, []));
   }
 
-  const result = concatenateArrays(["a", "b"], ["c", "d"], ["e", "f"]);
-  console.log(result);
+  concatenateArrays(["a", "b"], ["c", "d"], ["e", "f"]);
+}
+
+{
+  //Problem 4
+  class Vehicle {
+    private _make: string;
+    private _year: number;
+    constructor(_make: string, _year: number) {
+      this._make = _make;
+      this._year = _year;
+    }
+    getInfo(): string {
+      return `Make: ${this._make}, Year: ${this._year}`;
+    }
+  }
+
+  class Car extends Vehicle {
+    private _model: string;
+    constructor(_make: string, _year: number, _model: string) {
+      super(_make, _year);
+      this._model = _model;
+    }
+    getModel(): string {
+      return `Model: ${this._model}`;
+    }
+  }
+  const myCar = new Car("Toyota", 2020, "Corolla");
+  myCar.getInfo();
+  myCar.getModel();
 }
